906fdce7f8c5adc189c5819a9a3b76244dc33833
Mrityunjay-Palled <113463073+Mrityunjay-Palled@users.noreply.github.com>
2023-02-16 22:25:58 +0530

    added types and handling submit

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,9 +1,12 @@
+import { NoteData } from "./App"
 import { NoteForm } from "./NoteForm"
 export function NewNote(){
     return (
         <>
          <h1 className="mb-4">New Note</h1>
-         <NoteForm/>
+         <NoteForm onSubmit={function (data: NoteData): void {
+                throw new Error("Function not implemented.")
+            } }/>
         </>
     )
 }
